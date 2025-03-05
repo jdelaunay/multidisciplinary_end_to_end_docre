9e426b929db5f4787c1ab66e46d1e71ee04e2324
jdelaunay <jdelau01@univ-lr.fr>
2025-03-05 17:24:53 +0100

    Replace 512 by max_pos

--- model/layers/encoder.py
+++ model/layers/encoder.py
@@ -10,19 +10,23 @@ class Encoder(nn.Module):
         self.model = model
         self.transformer_type = transformer_type
         self.tokenizer = tokenizer
+        self.max_position_embeddings = self.model.config.max_position_embeddings
         if transformer_type == "bert":
             self.start_tokens = [self.tokenizer.cls_token_id]
             self.end_tokens = [self.tokenizer.sep_token_id]
         elif transformer_type == "roberta":
             self.start_tokens = [self.tokenizer.cls_token_id]
             self.end_tokens = [self.tokenizer.sep_token_id, self.tokenizer.sep_token_id]
+            self.max_position_embeddings = self.model.config.max_position_embeddings - 2
         else:
             raise ValueError(f"Unknown transformer type: {transformer_type}")
 
-
     def forward(self, input_ids, attention_mask):
         sequence_output, attention = self.process_long_input(
-            input_ids, attention_mask, start_tokens=self.start_tokens, end_tokens=self.start_tokens
+            input_ids,
+            attention_mask,
+            start_tokens=self.start_tokens,
+            end_tokens=self.start_tokens,
         )
         return sequence_output, attention
 
@@ -33,7 +37,7 @@ class Encoder(nn.Module):
         end_tokens = torch.tensor(end_tokens).to(input_ids)
         len_start = start_tokens.size(0)
         len_end = end_tokens.size(0)
-        if c <= 512:
+        if c <= self.max_position_embeddings:
             output = self.model(
                 input_ids=input_ids,
                 attention_mask=attention_mask,
@@ -45,20 +49,34 @@ class Encoder(nn.Module):
             new_input_ids, new_attention_mask, num_seg = [], [], []
             seq_len = attention_mask.sum(1).cpu().numpy().astype(np.int32).tolist()
             for i, l_i in enumerate(seq_len):
-                if l_i <= 512:
-                    new_input_ids.append(input_ids[i, :512])
-                    new_attention_mask.append(attention_mask[i, :512])
+                if l_i <= self.max_position_embeddings:
+                    new_input_ids.append(input_ids[i, : self.max_position_embeddings])
+                    new_attention_mask.append(
+                        attention_mask[i, : self.max_position_embeddings]
+                    )
                     num_seg.append(1)
                 else:
                     input_ids1 = torch.cat(
-                        [input_ids[i, : 512 - len_end], end_tokens], dim=-1
-                    )
-                    input_ids2 = torch.cat(
-                        [start_tokens, input_ids[i, (l_i - 512 + len_start) : l_i]],
+                        [
+                            input_ids[i, : self.max_position_embeddings - len_end],
+                            end_tokens,
+                        ],
                         dim=-1,
                     )
-                    attention_mask1 = attention_mask[i, :512]
-                    attention_mask2 = attention_mask[i, (l_i - 512) : l_i]
+                    input_ids2 = torch.cat(
+                        [
+                            start_tokens,
+                            input_ids[
+                                i,
+                                (l_i - self.max_position_embeddings + len_start) : l_i,
+                            ],
+                        ],
+                        dim=-1,
+                    )
+                    attention_mask1 = attention_mask[i, : self.max_position_embeddings]
+                    attention_mask2 = attention_mask[
+                        i, (l_i - self.max_position_embeddings) : l_i
+                    ]
                     new_input_ids.extend([input_ids1, input_ids2])
                     new_attention_mask.extend([attention_mask1, attention_mask2])
                     num_seg.append(2)
@@ -75,29 +93,62 @@ class Encoder(nn.Module):
             new_output, new_attention = [], []
             for n_s, l_i in zip(num_seg, seq_len):
                 if n_s == 1:
-                    output = F.pad(sequence_output[i], (0, 0, 0, c - 512))
-                    att = F.pad(attention[i], (0, c - 512, 0, c - 512))
+                    output = F.pad(
+                        sequence_output[i], (0, 0, 0, c - self.max_position_embeddings)
+                    )
+                    att = F.pad(
+                        attention[i],
+                        (
+                            0,
+                            c - self.max_position_embeddings,
+                            0,
+                            c - self.max_position_embeddings,
+                        ),
+                    )
                     new_output.append(output)
                     new_attention.append(att)
                 elif n_s == 2:
-                    output1 = sequence_output[i][: 512 - len_end]
-                    mask1 = attention_mask[i][: 512 - len_end]
-                    att1 = attention[i][:, : 512 - len_end, : 512 - len_end]
-                    output1 = F.pad(output1, (0, 0, 0, c - 512 + len_end))
-                    mask1 = F.pad(mask1, (0, c - 512 + len_end))
-                    att1 = F.pad(att1, (0, c - 512 + len_end, 0, c - 512 + len_end))
+                    output1 = sequence_output[i][
+                        : self.max_position_embeddings - len_end
+                    ]
+                    mask1 = attention_mask[i][: self.max_position_embeddings - len_end]
+                    att1 = attention[i][
+                        :,
+                        : self.max_position_embeddings - len_end,
+                        : self.max_position_embeddings - len_end,
+                    ]
+                    output1 = F.pad(
+                        output1, (0, 0, 0, c - self.max_position_embeddings + len_end)
+                    )
+                    mask1 = F.pad(
+                        mask1, (0, c - self.max_position_embeddings + len_end)
+                    )
+                    att1 = F.pad(
+                        att1,
+                        (
+                            0,
+                            c - self.max_position_embeddings + len_end,
+                            0,
+                            c - self.max_position_embeddings + len_end,
+                        ),
+                    )
 
                     output2 = sequence_output[i + 1][len_start:]
                     mask2 = attention_mask[i + 1][len_start:]
                     att2 = attention[i + 1][:, len_start:, len_start:]
-                    output2 = F.pad(output2, (0, 0, l_i - 512 + len_start, c - l_i))
-                    mask2 = F.pad(mask2, (l_i - 512 + len_start, c - l_i))
+                    output2 = F.pad(
+                        output2,
+                        (0, 0, l_i - self.max_position_embeddings + len_start, c - l_i),
+                    )
+                    mask2 = F.pad(
+                        mask2, (l_i - self.max_position_embeddings + len_start, c - l_i)
+                    )
                     att2 = F.pad(
                         att2,
                         [
-                            l_i - 512 + len_start,
+                            l_i - self.max_position_embeddings + len_start,
                             c - l_i,
-                            l_i - 512 + len_start,
+                            l_i - self.max_position_embeddings + len_start,
                             c - l_i,
                         ],
                     )
